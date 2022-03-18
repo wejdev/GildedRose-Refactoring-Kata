8e0200a41a1f1a70b2d279baddc21bbef6d497d4
billj <willard.johnson@eso.com>
2022-03-17 22:34:06 -0700

    Add tests around magic numbers in code. Didn't seem to matter much.

--- csharpcore/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -49,14 +49,34 @@ namespace GildedRoseTests
                 },
                 new object[]
                 {
-                    new List<Item> {new Item {Name = "foo", SellIn = 100, Quality = 100},
-                                    new Item {Name = "foo", SellIn =-40, Quality = -50}},
-                    new List<Item> {new Item {Name = "foo", SellIn = 99, Quality = 99},
-                                    new Item {Name = "foo", SellIn = -41, Quality = -50}}
+                    new List<Item> {
+                        new Item {Name = "foo", SellIn = 51, Quality = 51},
+                        new Item {Name = "foo", SellIn = 50, Quality = 50},
+                        new Item {Name = "foo", SellIn = 49, Quality = 49},
+                        new Item {Name = "foo", SellIn = 12, Quality = 12},
+                        new Item {Name = "foo", SellIn = 11, Quality = 11},
+                        new Item {Name = "foo", SellIn = 10, Quality = 10},
+                        new Item {Name = "foo", SellIn = 7, Quality = 7},
+                        new Item {Name = "foo", SellIn = 6, Quality = 6},
+                        new Item {Name = "foo", SellIn = 5, Quality = 5},
+                        new Item {Name = "foo", SellIn = 100, Quality = 100},
+                        new Item {Name = "foo", SellIn =-40, Quality = -50}},
+                    new List<Item> {
+                        new Item {Name = "foo", SellIn = 50, Quality = 50},
+                        new Item {Name = "foo", SellIn = 49, Quality = 49},
+                        new Item {Name = "foo", SellIn = 48, Quality = 48},
+                        new Item {Name = "foo", SellIn = 11, Quality = 11},
+                        new Item {Name = "foo", SellIn = 10, Quality = 10},
+                        new Item {Name = "foo", SellIn = 9, Quality = 9},
+                        new Item {Name = "foo", SellIn = 6, Quality = 6},
+                        new Item {Name = "foo", SellIn = 5, Quality = 5},
+                        new Item {Name = "foo", SellIn = 4, Quality = 4},         
+                        new Item {Name = "foo", SellIn = 99, Quality = 99},
+                        new Item {Name = "foo", SellIn = -41, Quality = -50}}
                 },
                 new object[]
                 {
-                    new List<Item> {new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 3, Quality = 5 } },
+                    new List<Item> {new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 3, Quality = 5 }},
                     new List<Item> {new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 3, Quality = 5}}
                 }
             };
