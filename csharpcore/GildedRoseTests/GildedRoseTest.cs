33ce53d1e024b80d92b54c2ff075858b8849cbfb
billj <willard.johnson@eso.com>
2022-03-17 23:25:22 -0700

    Added tests for existing rules and conditions

--- csharpcore/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -47,6 +47,18 @@ namespace GildedRoseTests
                     new List<Item> {new Item {Name = "foo", SellIn = -3, Quality = -5}},
                     new List<Item> {new Item {Name = "foo", SellIn = -4, Quality = -5}}
                 },
+                // Once the sell by date has passed, Quality degrades twice as fast
+                new object[]
+                {
+                    new List<Item> {new Item {Name = "foo", SellIn = -1, Quality = 24}},
+                    new List<Item> {new Item {Name = "foo", SellIn = -2, Quality = 22}}
+                },
+                // The Quality of an item is never negative
+                new object[]
+                {
+                    new List<Item> {new Item {Name = "foo", SellIn = 5, Quality = 0}},
+                    new List<Item> {new Item {Name = "foo", SellIn = 4, Quality = 0}}
+                },
                 new object[]
                 {
                     new List<Item> {
@@ -70,15 +82,105 @@ namespace GildedRoseTests
                         new Item {Name = "foo", SellIn = 9, Quality = 9},
                         new Item {Name = "foo", SellIn = 6, Quality = 6},
                         new Item {Name = "foo", SellIn = 5, Quality = 5},
-                        new Item {Name = "foo", SellIn = 4, Quality = 4},         
+                        new Item {Name = "foo", SellIn = 4, Quality = 4},
                         new Item {Name = "foo", SellIn = 99, Quality = 99},
                         new Item {Name = "foo", SellIn = -41, Quality = -50}}
                 },
+                // “Aged Brie” actually increases in Quality the older it gets
+                new object[]
+                {
+                    new List<Item> {new Item {Name = "Aged Brie", SellIn = -1, Quality = 5 }},
+                    new List<Item> {new Item {Name = "Aged Brie", SellIn = -2, Quality = 7 }}
+                },
+                new object[]
+                {
+                    new List<Item> {new Item {Name = "Aged Brie", SellIn = -22, Quality = 45 }},
+                    new List<Item> {new Item {Name = "Aged Brie", SellIn = -23, Quality = 47 }}
+                },
+                new object[]
+                {
+                    new List<Item> {new Item {Name = "Aged Brie", SellIn = -6, Quality = 48 }},
+                    new List<Item> {new Item {Name = "Aged Brie", SellIn = -7, Quality = 50 }}
+                },
+                new object[]
+                {
+                    new List<Item> {new Item {Name = "Aged Brie", SellIn = -6, Quality = 49 }},
+                    new List<Item> {new Item {Name = "Aged Brie", SellIn = -7, Quality = 50 }}
+                },
+                // The Quality of an item is never more than 50
+                new object[]
+                {
+                    new List<Item> {new Item {Name = "Aged Brie", SellIn = -6, Quality = 50 }},
+                    new List<Item> {new Item {Name = "Aged Brie", SellIn = -7, Quality = 50 }}
+                },
+                // "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
                 new object[]
                 {
                     new List<Item> {new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 3, Quality = 5 }},
-                    new List<Item> {new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 3, Quality = 5}}
-                }
+                    new List<Item> {new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 3, Quality = 5 }}
+                },
+                new object[]
+                {
+                    new List<Item> {new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -2, Quality = 0 }},
+                    new List<Item> {new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -2, Quality = 0 }}
+                },
+                new object[]
+                {
+                    new List<Item> {new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 55, Quality = 50 }},
+                    new List<Item> {new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 55, Quality = 50 }}
+                },
+                // "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+                // Quality increases by 2 when there are 10 days or less
+                new object[]
+                {
+                    new List<Item> {new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 11, Quality = 5 }},
+                    new List<Item> {new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 6}}
+                },
+                new object[]
+                {
+                    new List<Item> {new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 5 }},
+                    new List<Item> {new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 9, Quality = 7}}
+
+                },
+                new object[]
+                {
+                    new List<Item> {new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 9, Quality = 5 }},
+                    new List<Item> {new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 8, Quality = 7}}
+                },
+
+                // and by 3 when there are 5 days or less
+                new object[]
+                {
+                    new List<Item> {new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 6, Quality = 5 }},
+                    new List<Item> {new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 7}}
+                },
+                new object[]
+                {
+                    new List<Item> {new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 5 }},
+                    new List<Item> {new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 4, Quality = 8}}
+                },
+                new object[]
+                {
+                    new List<Item> {new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 4, Quality = 5 }},
+                    new List<Item> {new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 3, Quality = 8}}
+                },
+                // but Quality drops to 0 after the concert
+                new object[]
+                {
+                    new List<Item> {new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 1, Quality = 5 }},
+                    new List<Item> {new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 8}}
+                },
+                new object[]
+                {
+                    new List<Item> {new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 5 }},
+                    new List<Item> {new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = -1, Quality = 0}}
+                },
+                new object[]
+                {
+                    new List<Item> {new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = -1, Quality = 5 }},
+                    new List<Item> {new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = -2, Quality = 0 }}
+                },
+
             };
     }
 }
