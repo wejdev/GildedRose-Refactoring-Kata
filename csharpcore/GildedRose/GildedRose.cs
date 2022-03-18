1ed5c4ddcd21877c0bde5eb219094c882e0c1a55
billj <willard.johnson@eso.com>
2022-03-18 13:10:57 -0700

    Inline and remove red with logic on Aged Brie correct, now

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -5,7 +5,7 @@ namespace GildedRoseKata
 {
     public class GildedRose
     {
-        IList<Item> Items;
+        private readonly IList<Item> Items;
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
@@ -21,17 +21,10 @@ namespace GildedRoseKata
 
         private void UpdateQualityStuff(Item item)
         {
-            if (item.Name != "Aged Brie")
+            if (item.Name == "Aged Brie")
             {
                 if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (item.Quality > 0)
-                    {
-                        if (item.Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            item.Quality--;
-                        }
-                    }
                 }
                 else
                 {
@@ -41,21 +34,6 @@ namespace GildedRoseKata
 
                         if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                         {
-                            if (item.SellIn < 11)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality++;
-                                }
-                            }
-
-                            if (item.SellIn < 6)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality++;
-                                }
-                            }
                         }
                     }
                 }
@@ -69,19 +47,12 @@ namespace GildedRoseKata
                 {
                     if (item.Name != "Aged Brie")
                     {
-                        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                    }
+                    else
+                    {
+                        if (item.Quality < 50)
                         {
-                            if (item.Quality > 0)
-                            {
-                                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    item.Quality--;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            item.Quality = 0;
+                            item.Quality++;
                         }
                     }
                 }
@@ -100,6 +71,13 @@ namespace GildedRoseKata
         {
             if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
             {
+                if (item.Quality > 0)
+                {
+                    if (item.Name != "Sulfuras, Hand of Ragnaros")
+                    {
+                        item.Quality--;
+                    }
+                }
             }
             else
             {
@@ -109,6 +87,21 @@ namespace GildedRoseKata
 
                     if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
+                        if (item.SellIn < 11)
+                        {
+                            if (item.Quality < 50)
+                            {
+                                item.Quality++;
+                            }
+                        }
+
+                        if (item.SellIn < 6)
+                        {
+                            if (item.Quality < 50)
+                            {
+                                item.Quality++;
+                            }
+                        }
                     }
                 }
             }
@@ -122,13 +115,23 @@ namespace GildedRoseKata
             {
                 if (item.Name != "Aged Brie")
                 {
+                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (item.Quality > 0)
+                        {
+                            if (item.Name != "Sulfuras, Hand of Ragnaros")
+                            {
+                                item.Quality--;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        item.Quality = 0;
+                    }
                 }
                 else
                 {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality++;
-                    }
                 }
             }
         }
